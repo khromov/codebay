f539ecc4bd594a6181271daf7d01ce31c2764eed
Visya <m.verbenko@gmail.com>
2026-08-16 09:55:41 +0000

    feat: redraw bear avatar by hand

--- src/avatars/bear.ts
+++ src/avatars/bear.ts
@@ -2,13 +2,12 @@ import type { AvatarArt } from './types.ts';
 
 const art: AvatarArt = {
 	name: 'bear',
-	robot: true,
 	pixels: [
-		'##....##',
+		'###..###',
 		'###..###',
 		'########',
-		'##.##.##',
 		'########',
+		'##.##.##',
 		'########',
 		'###..###',
 		'.######.'
